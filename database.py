e4089872b1b101fec1ed1e0ec9cbb8b5739e0929
Yash Kulkarni <yash.kulkarni9597@gmail.com>
2019-02-10 23:13:09 +0530

    casual changes

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 def createTable():
     connection=sqlite3.connect("login.db")
     connection.execute("CREATE TABLE USERS(USERNAME TEXT NOT NULL,EMAIL TEXT,PASSWORD TEXT)")
-    connection.execute("INSERT INTO USERS VALUES(?,?,?)",('archana','archana@gmail.com','archangk'))
+    connection.execute("INSERT INTO USERS VALUES(?,?,?)",('yash','yash.kulkarni9597@gmail.com','9597'))
     connection.commit()
     result=connection.execute("SELECT * FROM USERS")
     
